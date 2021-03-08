543b2db2865edc9d5425357be822761bcf877d1e
MartinsMessias <messiasads2017@gmail.com>
2021-03-08 15:43:37 -0500

    make it better

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_caching import Cache
-from redis import Redis
+import redis
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
-cache = Redis(host='172.17.0.1', port=6379,
-              password='', decode_responses=True)
+cache = redis.from_url(os.environ.get("REDIS_URL"))
